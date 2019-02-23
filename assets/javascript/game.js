491aa9f1da54adee11b55972348f642c9fbefb4b
Bat <noreply>
2019-02-23 13:37:29 -0800

    homework

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,16 +7,19 @@ var losses = 0;
 //  generate a random number 
 
 
-var randomNum = Math.floor((Math.random() * 10) + 1); {
+var randomNum = Math.floor((Math.random() * 50) + 1); {
 
     $("#number").text(randomNum);
 
 }
 $(document).ready(function () {
-    $("#button-1").on("click", function () {
+    var randomNum1 = Math.floor((Math.random() * 10) + 1); {
+      
+        $("#button-1").on("click", function () {
+        $("#container1").text(randomNum1);
+        $("#container1" + randomNum1);
 
-
-    });
-
+        });
+    }
 
 });

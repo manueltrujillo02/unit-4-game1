2b7dbb0e1ab309416c22bfc1fb700b3f55586dcb
Bat <noreply>
2019-02-23 01:43:59 -0800

    homework

---
+++ assets/javascript/game.js
@@ -0,0 +1,22 @@
+
+// give the score of 0
+var wins = 0;
+var losses = 0;
+
+
+//  generate a random number 
+
+
+var randomNum = Math.floor((Math.random() * 10) + 1); {
+
+    $("#number").text(randomNum);
+
+}
+$(document).ready(function () {
+    $("#button-1").on("click", function () {
+
+
+    });
+
+
+});

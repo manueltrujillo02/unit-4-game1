1893e845bb8f7092d307c22cceca376ea28c7d17
Bat <noreply>
2019-02-25 19:02:45 -0800

    homework

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,50 +1,89 @@
-
-
-var wins = 0;
-var losses = 0;
-
-
-
-
-var yourGuessesText = document.getElementById("crystal2");
-
 var randomNum = Math.floor((Math.random() * 50) + 1); {
 
-    $("#number").text(randomNum);
-
-}
-
-$(document).ready(function () {
-   
-      
-        $("#button-1").on("click", function () {
-            var randomNum1 = Math.floor((Math.random() * 10) + 1); {
-                $("#crystal2").text(randomNum1);
-                
-                console.log(randomNum1)
+        $("#number").text(randomNum);
+    
+    }
+    
+    
+    $(document).ready(function () {
+        var randomNum1 = Math.floor((Math.random() * 10) + 1);
+        var randomNum2 = Math.floor((Math.random() * 10) + 1);
+        var randomNum3 = Math.floor((Math.random() * 10) + 1);
+        var randomNum4 = Math.floor((Math.random() * 10) + 1);
+        var wins = 0;
+        var losses = 0;
+        var result = 0;
+        var resultText = $("#yourguess");
+        $("#yourguess").text(result);
+    
+        $("#button1").on("click", function () {
+    
+            $("#yourguess").text(result += randomNum1);
+            console.log(randomNum1)
+    
+            if (result === randomNum) {
+                wins++
+                console.log(wins)
+                alert("you won")
+            }
+            if (result > randomNum) {
+                losses++
+                console.log(losses)
+                alert("you lost")
             }
         });
-        $("#button-2").on("click", function () {
-            var randomNum2 = Math.floor((Math.random() * 10) + 1); {
-                $("#crystal2").text(randomNum2);
-               
-                console.log(randomNum2)
+        
+        $("#button2").on("click", function () {
+    
+            $("#yourguess").text(result += randomNum2);
+            console.log(randomNum2)
+    
+            if (result === randomNum) {
+                wins++
+                console.log(wins)
+                alert("you won")
+            }
+            if (result > randomNum) {
+                losses++
+                console.log(losses)
+                alert("you lost")
+            }
+    
+        });
+       
+        $("#button3").on("click", function () {
+    
+            $("#yourguess").text(result += randomNum3);
+            console.log(randomNum3)
+    
+            if (result === randomNum) {
+                wins++
+                console.log(wins)
+                alert("you won")
+            }
+            if (result > randomNum) {
+                losses++
+                console.log(losses)
+                alert("you lost")
             }
         });
-        $("#button-3").on("click", function () {
-            var randomNum3 = Math.floor((Math.random() * 10) + 1); {
-                $("#crystal2").text(randomNum3);
-                console.log(randomNum3)
+       
+        $("#button4").on("click", function () {
+    
+            $("#yourguess").text(result += randomNum4);
+            console.log(randomNum4)
+    
+            if (result === randomNum) {
+                wins++
+                console.log(wins)
+                alert("you won")
             }
-        });
-        $("#button-4").on("click", function () {
-            var randomNum4 = Math.floor((Math.random() * 10) + 1); {
-                $("#crystal2").text(randomNum4);
-               
-                
-                console.log(randomNum4)
+            if (result > randomNum) {
+                losses++
+                console.log(losses)
+                alert("you lost")
             }
         });
     
-        yourGuessesText.textContent += randomNum1 + randomNum2 + randomNum3 + randomNum4;
-});
+    });
+    
